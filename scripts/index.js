573c187955b63b5ae972e38a8edb0cb427199950
ACuteWoof <sutharshanvithushan@gmail.com>
2021-04-27 13:22:36 +0530

    obtaining articles and redirecting using the github api 👍

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,45 @@
-console.log("built by ACuteWoof lol 🤣")
+// Grab MD info Json from => https://api.github.com/repos/engrafa/markdown/contents/
+// console.log("built by ACuteWoof lol 🤣");
+
+let temp = null;
+
+let articleJson = null;
+
+
+function requestMarkdownArticles(username = "", repo = ""){
+
+    const xhr = new XMLHttpRequest();
+    
+    const url = `https://api.github.com/repos/${username}/${repo}/contents/`
+
+    xhr.open('GET', url, true);
+
+    xhr.onload = function() {
+    
+        // Parse API data into JSON
+        const data = JSON.parse(this.response);
+        
+        // Log the response
+        console.log(data);
+
+        for (let i in data){
+            console.log("name:", data[i].name)
+            console.log("url:", data[i].download_url)
+            let articleGrid = document.getElementById("articleGrid");
+            articleGrid.innerHTML = articleGrid.innerHTML + `<div class="article_box" onclick='window.open("${data[i].html_url}", "_blank");'><h4>${data[i].name}</h4><img src="https://picsum.photos/200"></div>`;
+        }
+    }
+
+    xhr.send();
+
+}
+
+requestMarkdownArticles("engrafa", "markdown");
+
+// fetch("https://api.github.com/repos/engrafa/markdown/contents/").then(jsonRaw=>{
+//     // articleJson = JSON.parse(JSON.stringify(jsonRaw.json()));
+//     console.log(JSON.stringify(jsonRaw.json()))
+// })
+
+// articleGrid = document.getElementById("articleGrid");
+// articleGrid.innerHTML = '<div class="article_box"><h4>' + '</h4></div>';
