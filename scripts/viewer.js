96b0401327d3d6071aace8fe45b28d31e6c1a8cb
ACuteWoof <sutharshanvithushan@gmail.com>
2021-04-28 14:48:30 +0530

    script

--- scripts/viewer.js
+++ scripts/viewer.js
@@ -2,17 +2,22 @@ const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 const article = urlParams.get('article')
 
-document.getElementById('content').innerHTML = marked('# Marked in the browser\n\nRendered by **marked**.');
 
 if (String(article) != ''){
     const mdUrl = `https://raw.githubusercontent.com/engrafa/markdown/main/${article}`
-    // viewArticle(mdUrl)
+    viewArticle(mdUrl, 'content')
 }
 
-function viewArticle(url){
-    // If you're in the browser, the Remarkable class is already available in the window
-    var md = new Remarkable();
+function viewArticle(url, element = 'content'){
+    const xhr = new XMLHttpRequest();
 
-    // Outputs: <h1>Remarkable rulezz!</h1>
-    console.log(md.render('# Remarkable rulezz!'));
+    xhr.open('GET', url, true);
+
+    xhr.onload = function() {
+        const data = this.response
+        document.getElementById(element).innerHTML = marked();
+
+    }
+
+    xhr.send();
 }
